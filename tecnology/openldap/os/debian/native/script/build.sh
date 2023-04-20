0bad12bad53af9b70cbef6afcfe80f6dcbf44953
rick0x00 <rick.0x00@gmail.com>
2023-04-19 21:54:09 -0300

    add basic script to build

---
+++ tecnology/openldap/os/debian/native/script/build.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# ============================================================ #
+# Tool Created date: 19 abr 2023                               #
+# Tool Created by: Henrique Silva (rick.0x00@gmail.com)        #
+# Tool Name: srv_ldap                                          #
+# Description: script for help to creation ldap server         #
+# License: MIT License                                         #
+# Remote repository 1: https://github.com/rick0x00/srv_ldap    #
+# Remote repository 2: https://gitlab.com/rick0x00/srv_ldap    #
+# ============================================================ #
+
+# ============================================================ #
+# start root user checking
+if [ $(id -u) -ne 0 ]; then
+    echo "Please use root user to run the script."
+    exit 1
+fi
+# end root user checking
+# ============================================================ #
+# start set variables
+
+# end set variables
+# ============================================================ #
+# start definition functions
+# ============================== #
+# start complement functions
+
+# end complement functions
+# ============================== #
+# start main functions
+
+# end main functions
+# ============================== #
+# end definition functions
+# ============================================================ #
+# start argument reading
+
+# end argument reading
+# ============================================================ #
+# start main executions of code
+
